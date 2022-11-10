b7af02df03e6ea9763c3dac6f0bd93d91c74741d
Chris Mems <christopher.mems@.com>
2022-11-10 11:41:09 -0600

    center information on contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,13 @@
+import "../styles/Contact.css"
+
 const Contact = () => {
   return (
-  <>
+  <div class='contact'>
     <h1>Reach out if you'd like to work with me!</h1>
     <a href="mailto:Christopher.mems@yahoo.com">Christopher.mems@yahoo.com</a>
     <a href="http://www.linkedin.com/in/christopher-mems">LinkedIn</a>
     <a href="https://github.com/cmthecoder">GitHub</a>
-  </>
+  </div>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div class="home-view">
       <h1>Christopher Mems</h1>
-      <img src="https://i.imgur.com/pqyfJ16.jpg?2" alt=""/>
+      <img src="https://i.imgur.com/pqyfJ16.jpg?2" width="500" alt=""/>
       <h2>Software Engineer</h2>
     </div>
   );
