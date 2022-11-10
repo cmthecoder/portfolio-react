ffd99a060ab3827854ba91918e2acdf0c2a086d9
Chris Mems <christopher.mems@.com>
2022-11-10 14:17:27 -0600

    edit css

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div class="about">
       <img src="https://i.imgur.com/pqyfJ16.jpg?2" alt=""/>
-      <h2>I am a Software Engineer with experience in coding languages such as CSS, HTML, JavaScript, and React. I interact with clients to gather key details so that I may use my engineering skills to turn their ideas into reality. With my passion for helping people and solving problems, Software Engineering gives me the ability to create change and be a contributor to a team in generating solutions.</h2>
+      <h2 class="about-text">I am a Software Engineer with experience in coding languages such as CSS, HTML, JavaScript, and React. I interact with clients to gather key details so that I may use my engineering skills to turn their ideas into reality. With my passion for helping people and solving problems, Software Engineering gives me the ability to create change and be a contributor to a team in generating solutions.</h2>
     </div>
   );
 }
