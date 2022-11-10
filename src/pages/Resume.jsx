3d826a8601d9e73812e67b5f4ec39336bc6efc47
Chris Mems <christopher.mems@.com>
2022-11-10 11:46:00 -0600

    center elements in pages

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,12 @@
+import "../styles/Resume.css"
+
 const Resume = () => {
   return (
-    <>
+    <div class="resume">
       <h1>Resume</h1>
       <p>To download my resume, click this <a href="Christopher Mems Resume.pdf" download>link.</a>
       </p>
-    </>
+    </div>
   );
 }
 
