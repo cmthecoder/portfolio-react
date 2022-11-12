0830594dc54271d3396c45b4aef95de5378b072a
Chris Mems <christopher.mems@.com>
2022-11-12 11:47:51 -0600

    add css

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,10 @@
 import { projects } from "../data/projects";
 import ProjectsList from "../components/ProjectsList";
+import '../styles/Projects.css'
 
 const Projects = (props) => {
   return (
-    <div>
+    <div class='projects'>
       <ProjectsList projects={projects}/>
     </div>
   );
