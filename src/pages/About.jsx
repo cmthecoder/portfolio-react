020b5f75345d7974e0e20bb2f7fd5fd14fe4f6c9
Chris Mems <christopher.mems@.com>
2022-11-22 13:08:36 -0600

    add vh to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ import '../styles/About.css'
 const About = () => {
   return (
     <div className="about">
-      <img src="./IMG_7119.png" width='400' alt=""/>
+      <img src="./IMG_7119.png" width='28%' alt=""/>
       <h2 className="about-text animate__animated animate__lightSpeedInRight">I am a Software Engineer with experience in coding languages such as CSS, HTML, JavaScript, React, and Python. I interact with clients to gather key details so that I may use my engineering skills to turn their ideas into reality. With my passion for helping people and solving problems, Software Engineering gives me the ability to create change and be a contributor to a team in generating solutions.</h2>
     </div>
   );
