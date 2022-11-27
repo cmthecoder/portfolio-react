bf063150948b10b4159394513b20cf3558dbe2ad
Chris Mems <christopher.mems@.com>
2022-11-27 16:50:26 -0600

    add the finch collector project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,10 +1,17 @@
 const projects = [
   {
-    title: "Tic-Tac-Toe",
-    description: "A two player game of X's and O's where the winner is the first player to have either 3 X's or 3 O's in a row.",
-    image: "https://i.imgur.com/iWkvNxN.png",
-    repositoryLink: "https://github.com/cmthecoder/ttt-weekend.git",
-    deploymentLink: "https://chris-mems-ttt.netlify.app/",
+    title: "The Finch Collector",
+    description: "The Finch Collector is a fun app for those who want to share something about finches that they have or have seen.",
+    image: "https://i.imgur.com/qTWX38R.png",
+    repositoryLink: "https://github.com/cmthecoder/finch-collector",
+    deploymentLink: "https://chris-mems-finch-collector.herokuapp.com/",
+  },
+  {
+    title: "The Flyers Club",
+    description: "The Flyers Club is a community where people who share a passion for aviation can meet and share their fun adventures or discuss changes in the world of aviation.",
+    image: "https://i.imgur.com/O42sZtZ.png",
+    repositoryLink: "https://github.com/cmthecoder/the-flyers-club.git",
+    deploymentLink: "https://the-flyers-club-chris-mems.fly.dev/",
   },
   {
     title: "Connect Four",
@@ -14,11 +21,11 @@ const projects = [
     deploymentLink: "https://cmems-connect-four.netlify.app/",
   },
   {
-    title: "The Flyers Club",
-    description: "The Flyers Club is a community where people who share a passion for aviation can meet and share their fun adventures or discuss changes in the world of aviation.",
-    image: "https://i.imgur.com/O42sZtZ.png",
-    repositoryLink: "https://github.com/cmthecoder/the-flyers-club.git",
-    deploymentLink: "https://the-flyers-club-chris-mems.fly.dev/",
+    title: "Tic-Tac-Toe",
+    description: "A two player game of X's and O's where the winner is the first player to have either 3 X's or 3 O's in a row.",
+    image: "https://i.imgur.com/iWkvNxN.png",
+    repositoryLink: "https://github.com/cmthecoder/ttt-weekend.git",
+    deploymentLink: "https://chris-mems-ttt.netlify.app/",
   },
 ]
 
