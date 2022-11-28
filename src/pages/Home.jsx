518e91e893028803f0dfe26ddd9e0f1cac5a60b1
Chris Mems <christopher.mems@.com>
2022-11-27 20:47:27 -0600

    change body to a div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,13 @@ import "../styles/Home.css"
 
 const Home = () => {
   return (
-    <body>
+    <div>
       <div className="home-view">
         <h1 className='my-name'>Christopher Mems</h1>
         <img src="https://i.imgur.com/0zmtqgP.jpg" width="450" alt=""/>
         <h2 className="role">Software Engineer</h2>
       </div>
-    </body>
+    </div>
   );
 }
 
